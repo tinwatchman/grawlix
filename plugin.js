ec7e826c3ad3eaec0e6c05f17d1dd206bb2ad451
tinwatchman <j.stout@jonstout.net>
2017-03-27 20:17:17 -0400

    add GrawlixPlugin class and spec

---
+++ plugin.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/**
+ * Contains the specification class for grawlix plugins
+ */
+
+const _ = require('underscore');
+
+/**
+ * Defines a grawlix plugin.
+ * @param {Object}   obj         Constructor options. Optional.
+ * @param {Array}    obj.filters Array of filter objects. Optional.
+ * @param {Array}    obj.styles  Array of grawlix styles. Optional.
+ * @param {Function} obj.init    Initialization function override. Optional.
+ */
+var GrawlixPlugin = function(obj) {
+  /**
+   * Array of filter objects
+   * @type {Array}
+   */
+  this.filters = [];
+
+  /**
+   * Array of GrawlixStyle objects
+   * @type {Array}
+   */
+  this.styles = [];
+
+  /**
+   * Optional initialization function. Called when plugin first loaded. Can be
+   * overridden in subclasses or via the `obj` argument. Value of `this` inside 
+   * the function will always be the `GrawlixPlugin` instance.
+   * @param  {Object} options Options object of the main `grawlix` instance.
+   * @return {void}
+   */
+  this.init = function(options) {
+    // can override in subclasses
+  };
+
+  if (!_.isUndefined(obj) && _.has(obj, 'filters') && _.isArray(obj.filters)) {
+    this.filters = obj.filters;
+  }
+  if (!_.isUndefined(obj) && _.has(obj, 'styles') && _.isArray(obj.styles)) {
+    this.styles = obj.styles;
+  }
+  if (!_.isUndefined(obj) && _.has(obj, 'init') && _.isFunction(obj.init)) {
+    this.init = _.bind(obj.init, this);
+  }
+};
+GrawlixPlugin.prototype = {};
+
+/**
+ * Export
+ */
+module.exports = GrawlixPlugin;
---
+++ spec/plugin-spec.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const GrawlixPlugin = require('../plugin');
+const GrawlixStyle = require('../styles').GrawlixStyle;
+const _ = require('underscore');
+
+describe('GrawlixPlugin', function() {
+  describe('#constructor', function() {
+    it('should be fine without any arguments provided', function() {
+      var plugin = new GrawlixPlugin();
+      expect(plugin).toBeDefined();
+      expect(plugin.filters).toBeDefined();
+      expect(_.isArray(plugin.filters)).toBe(true);
+      expect(plugin.styles).toBeDefined();
+      expect(_.isArray(plugin.styles)).toBe(true);
+      expect(plugin.init).toBeDefined();
+      expect(_.isFunction(plugin.init)).toBe(true);
+    });
+    it('should accept a filters option (if an array)', function() {
+      var filter = { word: 'word', pattern: /word/i };
+      var filter2 = { word: 'word2', pattern: /word2/i };
+      var plugin = new GrawlixPlugin({
+        filters: [ filter, filter2 ]
+      });
+      var plugin2 = new GrawlixPlugin({
+        filters: {
+          filter: filter,
+          filter2: filter2
+        }
+      });
+      expect(plugin.filters.length).toBe(2);
+      expect(plugin.filters[0]).toBe(filter);
+      expect(plugin.filters[1]).toBe(filter2);
+      expect(plugin2.filters.length).toBe(0);
+    });
+    it('should accept a styles option (if an array)', function() {
+      var javaStyle = new GrawlixStyle('java', '☕');
+      var rookStyle = new GrawlixStyle('rook', '♜');
+      var plugin = new GrawlixPlugin({
+        styles: [
+          javaStyle,
+          rookStyle
+        ]
+      });
+      var plugin2 = new GrawlixPlugin({
+        styles: {
+          0: javaStyle,
+          1: rookStyle
+        }
+      });
+      expect(plugin.styles.length).toBe(2);
+      expect(plugin.styles[0]).toBe(javaStyle);
+      expect(plugin.styles[1]).toBe(rookStyle);
+      expect(plugin2.styles.length).toBe(0);
+    });
+    it('should accept an init function (if a function)', function() {
+      var isChanged = false;
+      var init = function(options) {
+        isChanged = true;
+      };
+      var plugin = new GrawlixPlugin({
+        init: init
+      });
+      var plugin2 = new GrawlixPlugin({
+        init: {} // should ignore this, keep the default
+      });
+      expect(plugin.init).toBeDefined();
+      expect(_.isFunction(plugin.init)).toBe(true);
+      expect(plugin2.init).toBeDefined();
+      expect(_.isFunction(plugin2.init)).toBe(true);
+      plugin.init();
+      plugin2.init();
+      expect(isChanged).toBe(true);
+    });
+  });
+  
+  describe('#init', function() {
+    it('should always have `this` set to the plugin instance', function() {
+      var plugin = new GrawlixPlugin({
+        init: function(options) {
+          options.x++;
+          options.self = this;
+        }
+      });
+      var opts = { x: 0 };
+      plugin.init(opts);
+      expect(opts.x).toEqual(1);
+      expect(opts.self).toBeDefined();
+      expect(opts.self).toBe(plugin);
+    });
+  });
+});
